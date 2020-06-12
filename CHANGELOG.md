03d81507dfd39fc3adcb4a412160d650c020e801
Ricky Angeles <ricky@improveandgrow.com>
2020-06-12 14:11:07 -0400

    0.7.6

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,11 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ## [Unreleased]
 
 
+## [0.7.6] - 2020-06-12
+
+## Fixed
+- Theme: Minor UI changes
+
 ## [0.7.5] - 2020-06-12
 
 ### Fixed
--- single-documentation.php
+++ single-documentation.php
@@ -21,13 +21,7 @@ get_header();
 		<?php
 		while ( have_posts() ) :
 			the_post();
-
-			if ( is_singular('documentation') {
 				get_template_part( 'template-parts/content', 'documentation' );
-			} else {
-				get_template_part( 'template-parts/content', 'page' );
-			}
-
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) :
 				comments_template();
